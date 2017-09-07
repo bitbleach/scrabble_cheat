54f79df0d0ca83ccba77c7ea1efcdf98700140e2
Alex <ageorge010@vt.edu>
2017-09-07 22:00:40 +0000

    stores bogus defs in cookie if blank space exists on fake word, still need to make cookie expire

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,23 @@
 class PagesController < ApplicationController
-  # GET /
-  #
-  def main
-  end
-  
+
   def search
     @term = params[:search][:query]
+    seed_fake_definition if @term.first.blank?
+    @definitions = cookies[@term].blank? ? map_definitions(@term) : JSON.parse(cookies[@term]) 
+  end
+  
+  private
+  
+  def seed_fake_definition
+    @term.strip!
+    cookies[@term] = fake_definitions if cookies[@term].blank?
+  end
+  
+  def map_definitions(word)
+     Wordnik.word.get_definitions(word, limit: 6).map{ |t| t['text'] }
+  end
+    
+  def fake_definitions
+    JSON.generate((1..rand(1..4)).flat_map{ LiterateRandomizer.sentence })
   end
 end
